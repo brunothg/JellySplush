69a0460b81b7bdfd2ef7c51357ab8dd76ceddc76
brunothg <android.bruns@googlemail.com>
2015-03-12 21:08:20 +0100

    laf images setup

--- src/java/de/bno/jellysplush/Main.java
+++ src/java/de/bno/jellysplush/Main.java
@@ -1,9 +1,32 @@
 package de.bno.jellysplush;
 
+import game.engine.image.InternalImage;
+
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 public class Main {
+
+	public static final String IMAGE_PATH = "/de/bno/jellysplush/images/";
 
 	public static void main(String[] args) {
 
+		setLaF();
+		setupInternalImage();
+	}
+
+	private static void setupInternalImage() {
+
+		InternalImage.setRootFolder(IMAGE_PATH);
+	}
+
+	private static void setLaF() {
+
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException
+				| IllegalAccessException | UnsupportedLookAndFeelException e) {
+		}
 	}
 
 }
