c25a2bd5d85a99a6b3ab5e405e0ada5e0788c087
brunothg <android.bruns@googlemail.com>
2015-04-09 17:39:42 +0200

    PowerupUtils

---
+++ src/java/de/bno/jellysplush/data/powerup/PowerupUtils.java
@@ -0,0 +1,46 @@
+package de.bno.jellysplush.data.powerup;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class PowerupUtils {
+
+	private static final List<Powerup> powerups = new ArrayList<Powerup>();
+
+	static {
+
+		powerups.add(new SpeedPowerup());
+	}
+
+	public static List<Powerup> getPowerups() {
+
+		List<Powerup> ret = new ArrayList<Powerup>(powerups.size());
+		Collections.copy(ret, powerups);
+
+		return ret;
+	}
+
+	public static Powerup[] getRandomPowerups(int anz) {
+
+		Collections.shuffle(powerups);
+
+		Powerup[] ret = new Powerup[anz];
+
+		int index = 0;
+
+		while (index < ret.length) {
+			for (Powerup power : powerups) {
+
+				if (index >= ret.length) {
+					break;
+				}
+
+				ret[index] = power;
+				index++;
+			}
+		}
+
+		return ret;
+	}
+}
