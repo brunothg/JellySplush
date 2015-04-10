19efae10d9781a7df5f9193a25a7d5d7e105247e
brunothg <android.bruns@googlemail.com>
2015-04-10 19:13:53 +0200

    Lifepowerup

---
+++ src/java/de/bno/jellysplush/data/powerup/powerups/LifePowerup.java
@@ -0,0 +1,35 @@
+package de.bno.jellysplush.data.powerup.powerups;
+
+import game.engine.time.TimeUtils;
+import de.bno.jellysplush.Constants;
+import de.bno.jellysplush.Settings;
+import de.bno.jellysplush.data.JellyFish;
+import de.bno.jellysplush.data.powerup.DefaultPowerup;
+import de.bno.jellysplush.data.powerup.Powerup;
+
+public class LifePowerup extends DefaultPowerup {
+
+	private static final double EXTENDED_LIFETIME = 0.5;
+
+	public LifePowerup() {
+
+		super(5);
+		setLifetimeOnBoard(Math.round(TimeUtils.NanosecondsOfSeconds(Settings
+				.getInt(Settings.KEY_POWERUP_LIFETIME,
+						Constants.POWERUP_LIFETIME_DEFAULT))
+				* EXTENDED_LIFETIME));
+	}
+
+	@Override
+	public void manipulateOwnJellyFish(JellyFish fish) {
+
+		fish.setLifes(fish.getLifes() + 1);
+	}
+
+	@Override
+	public Powerup clone() {
+
+		return new LifePowerup();
+	}
+
+}
