c83d554d3df8203d4dd889c5825e2033ea25fa86
brunothg <android.bruns@googlemail.com>
2015-04-10 17:01:32 +0200

    slow down change

--- src/java/de/bno/jellysplush/data/powerup/powerups/SlowPowerup.java
+++ src/java/de/bno/jellysplush/data/powerup/powerups/SlowPowerup.java
@@ -5,28 +5,24 @@ import de.bno.jellysplush.data.JellyFish;
 import de.bno.jellysplush.data.powerup.DefaultPowerup;
 import de.bno.jellysplush.data.powerup.Powerup;
 
-public class SlowPowerup extends DefaultPowerup
-{
+public class SlowPowerup extends DefaultPowerup {
 
-	private static double SPEED_PLUS = -0.15;
+	private static double SPEED_PLUS = -0.25;
 	private static double MIN_SPEED = Constants.MIN_REL_SPEED;
 
-	public SlowPowerup()
-	{
+	public SlowPowerup() {
 
 		super(1);
 	}
 
 	@Override
-	public void manipulateOwnJellyFish(JellyFish fish)
-	{
+	public void manipulateOwnJellyFish(JellyFish fish) {
 
 		fish.setSpeed(Math.max(MIN_SPEED, fish.getSpeed() + SPEED_PLUS));
 	}
 
 	@Override
-	public Powerup clone()
-	{
+	public Powerup clone() {
 		return new SlowPowerup();
 	}
 }
