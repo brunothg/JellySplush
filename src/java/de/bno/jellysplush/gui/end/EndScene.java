5db285e499a25a8f53e80f42392db2e6ebffae97
Marvin Bruns <android.bruns@googlemail.com>
2015-04-16 13:50:27 +0200

    txt

--- src/java/de/bno/jellysplush/gui/end/EndScene.java
+++ src/java/de/bno/jellysplush/gui/end/EndScene.java
@@ -17,7 +17,10 @@ import java.util.EventListener;
 import de.bno.jellysplush.data.JellyFish;
 import de.bno.jellysplush.gui.start.ColorPicker;
 
-public class EndScene implements Scene, KeyListener {
+public class EndScene implements Scene, KeyListener
+{
+
+	private static final String WIN_TEXT = "The winner is, with %d points and %d health ...";
 
 	private static final double COLOR_SIZE = 0.5;
 
@@ -34,20 +37,20 @@ public class EndScene implements Scene, KeyListener {
 
 	private ColorPicker cp = new ColorPicker(TEXT_COLOR);
 
-	public EndScene(JellyFish fish1, JellyFish fish2, boolean rightIsWinner) {
+	public EndScene(JellyFish fish1, JellyFish fish2, boolean rightIsWinner)
+	{
 
-		if (rightIsWinner) {
+		if (rightIsWinner)
+		{
 
-			winText = String.format(
-					"The winner is, with %d points and %d deaths ...",
-					fish2.getPoints(), -fish2.getLifes());
+			winText = String.format(WIN_TEXT, fish2.getPoints(), fish2.getLifes());
 
 			cp.setColor(fish2.getColor());
-		} else {
+		}
+		else
+		{
 
-			winText = String.format(
-					"The winner is, with %d points and %d deaths ...",
-					fish1.getPoints(), -fish1.getLifes());
+			winText = String.format(WIN_TEXT, fish1.getPoints(), fish1.getLifes());
 
 			cp.setColor(fish1.getColor());
 		}
@@ -56,21 +59,21 @@ public class EndScene implements Scene, KeyListener {
 	}
 
 	@Override
-	public void paintScene(Graphics2D g, int width, int height, long elapsedTime) {
+	public void paintScene(Graphics2D g, int width, int height, long elapsedTime)
+	{
 
-		g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
-				RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+		g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
 
 		drawBackground(g, width, height);
 		drawStatus(g, width, height);
 	}
 
-	private void drawStatus(Graphics2D g, int width, int height) {
+	private void drawStatus(Graphics2D g, int width, int height)
+	{
 
 		cp.setSize((int) (width * COLOR_SIZE), (int) (height * COLOR_SIZE));
-		cp.setTopLeftPosition(new Point(
-				(int) (width * 0.5 - cp.getWidth() * 0.5),
-				(int) (height * 0.5 - cp.getHeight() * 0.5)));
+		cp.setTopLeftPosition(new Point((int) (width * 0.5 - cp.getWidth() * 0.5),
+			(int) (height * 0.5 - cp.getHeight() * 0.5)));
 		cp.paintOnScene(g, 0);
 
 		int _width = width;
@@ -83,11 +86,11 @@ public class EndScene implements Scene, KeyListener {
 		int mHeight = metrics.getMaxAscent() + metrics.getMaxDescent();
 		int mWidth = metrics.stringWidth(winText);
 
-		while (mHeight > _height || mWidth > _width) {
+		while (mHeight > _height || mWidth > _width)
+		{
 
 			oldFont = g.getFont();
-			g.setFont(new Font(oldFont.getName(), oldFont.getStyle(), oldFont
-					.getSize() - 1));
+			g.setFont(new Font(oldFont.getName(), oldFont.getStyle(), oldFont.getSize() - 1));
 
 			metrics = g.getFontMetrics();
 			mHeight = metrics.getMaxAscent() + metrics.getMaxDescent();
@@ -95,19 +98,23 @@ public class EndScene implements Scene, KeyListener {
 		}
 
 		g.setColor(TEXT_COLOR);
-		g.drawString(winText, (float) (_width * 0.5 - mWidth * 0.5),
-				(float) (_height * 0.5 - mHeight * 0.5));
+		g.drawString(winText, (float) (_width * 0.5 - mWidth * 0.5), (float) (_height * 0.5 - mHeight * 0.5));
 	}
 
-	private void drawBackground(Graphics2D g2d, int width, int height) {
+	private void drawBackground(Graphics2D g2d, int width, int height)
+	{
 
 		boolean cSwitch = true;
 
-		for (int i = 0; i < height; i += BACKGROUND_HEIGHT) {
+		for (int i = 0; i < height; i += BACKGROUND_HEIGHT)
+		{
 
-			if (cSwitch) {
+			if (cSwitch)
+			{
 				g2d.setColor(BACKGROUND_COLOR_1);
-			} else {
+			}
+			else
+			{
 				g2d.setColor(BACKGROUND_COLOR_2);
 			}
 
@@ -118,46 +125,53 @@ public class EndScene implements Scene, KeyListener {
 	}
 
 	@Override
-	public EventListener[] getEventListeners() {
+	public EventListener[] getEventListeners()
+	{
 		return new EventListener[] { this };
 	}
 
-	public ActionListener getActionListener() {
+	public ActionListener getActionListener()
+	{
 		return actionListener;
 	}
 
-	public void setActionListener(ActionListener actionListener) {
+	public void setActionListener(ActionListener actionListener)
+	{
 		this.actionListener = actionListener;
 	}
 
-	private void fireRestartEvent() {
+	private void fireRestartEvent()
+	{
 
-		if (getActionListener() != null) {
-			getActionListener().actionPerformed(
-					new ActionEvent(this, ActionEvent.ACTION_PERFORMED,
-							"restart"));
+		if (getActionListener() != null)
+		{
+			getActionListener().actionPerformed(new ActionEvent(this, ActionEvent.ACTION_PERFORMED, "restart"));
 		}
 	}
 
 	@Override
-	public void keyTyped(KeyEvent e) {
+	public void keyTyped(KeyEvent e)
+	{
 	}
 
 	@Override
-	public void keyPressed(KeyEvent e) {
+	public void keyPressed(KeyEvent e)
+	{
 	}
 
 	@Override
-	public void keyReleased(KeyEvent e) {
+	public void keyReleased(KeyEvent e)
+	{
 
 		int keyCode = e.getKeyCode();
 
-		switch (keyCode) {
+		switch (keyCode)
+		{
 
-		case KeyEvent.VK_ESCAPE:
-		case KeyEvent.VK_ENTER:
+			case KeyEvent.VK_ESCAPE:
+			case KeyEvent.VK_ENTER:
 
-			fireRestartEvent();
+				fireRestartEvent();
 			break;
 		}
 	}
